b8a828935273d85c8ddbed10df916e278d09a902
Alex Johnson <alex.johnson293@gmail.com>
2022-11-14 13:45:56 -0800

    revise test to correct incorrect jest testing language

--- __tests__/expectancy.test.js
+++ __tests__/expectancy.test.js
@@ -4,19 +4,17 @@ describe('Expectancy', () => {
   let expectancy;
 
   beforeEach(() => {
-    expectancy = new Expectancy(false, 150,  1, 8, false, "high");
+    expectancy = new Expectancy(false, 150, 1, 8, false, "high");
   });
 
-  test('should create an Expectancy object')
-  expect(expectancy.smoker).toEqual(any(Boolean));
-  expect(expectancy.weight).toEqual(any(Number));
-  expect(expectancy.married).toBeGreaterThanOrEqualTo(0);
-  expect(expectancy.married).toBeLessThanOrEqualTo(2);
-  expect(expectancy.drinks).toBeGreaterThanOrEqualTo(0);
-  expect(expectancy.drinks).toBeLessThanOrEqualTo(10);
-  expect(expectancy.major).toEqual(any(Boolean));
-  expect(expectancy.exercise).toEqual("high")
-  
+  test('should create an Expectancy object', () => {
+    expect(expectancy.smoker).toEqual(false);
+    expect(expectancy.weight).toEqual(150);
+    expect(expectancy.married).toEqual(1);
+    expect(expectancy.drinks).toEqual(8);
+    expect(expectancy.majorCondition).toEqual(false);
+    expect(expectancy.exercise).toEqual("high");
+  });
 
 
 
