26c15e4dd6e41fea53b887dbcc2451226552ea18
Alex Johnson <alex.johnson293@gmail.com>
2022-11-14 13:54:27 -0800

    passes expectancy creation

---
+++ src/js/expectancy.js
@@ -0,0 +1,10 @@
+export default class Expectancy {
+  constructor(smoker, weight, married, drinks, majorCondition, exercise) {
+    this.smoker = smoker;
+    this.weight = weight;
+    this.married = married;
+    this.drinks = drinks;
+    this.majorCondition = majorCondition;
+    this.exercise = exercise; 
+  }
+}
