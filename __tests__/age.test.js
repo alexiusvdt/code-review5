fca848cef152de3a2fe64a8cecfe43f4f1908e54
Alex Johnson <alex.johnson293@gmail.com>
2022-11-14 08:38:37 -0800

    add test for average life expectancy

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -4,12 +4,15 @@ describe('Age', () => {
   let age;
 
   beforeEach(() => {
-    age = new Age(30);
+    age = new Age(30,80);
   });
   
-  test('should take user input of earth age and store in an object', () => {
+  test('should take user input of earth age and store in Age object', () => {
     expect(age.earth).toEqual(30);
   });
 
+  test('should take user input of earth life expectancy and store in Age object', () => {
+    expect(age.averageEarth).toEqual(80)
+  })
 
 });
