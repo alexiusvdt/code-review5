f580e58540360fb7f2c71a7590b446ee1130b14a
Alex Johnson <alex.johnson293@gmail.com>
2022-11-14 09:38:31 -0800

    add marsAge test

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -12,15 +12,19 @@ describe('Age', () => {
   });
 
   test('should take user input of earth life expectancy and store in Age object', () => {
-    expect(age.averageEarth).toEqual(80)
+    expect(age.averageEarth).toEqual(80);
   });
 
   test('should return user age in Mercury years', () => {
-    expect(age.mercuryAge()).toEqual(125)
+    expect(age.mercuryAge()).toEqual(125);
   });
 
   test('should return user age in Venus years', () => {
-    expect(age.venusAge()).toEqual(48.39)
+    expect(age.venusAge()).toEqual(48.39);
+  });
+
+  test('should return user age in Mars years', () => {
+    expect(age.marsAge()).toEqual(15.96);
   });
 
 });
