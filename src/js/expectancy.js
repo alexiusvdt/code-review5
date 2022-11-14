8bd80224a0668b082c2be6c7d9fd82a9d64de470
Alex Johnson <alex.johnson293@gmail.com>
2022-11-14 14:34:14 -0800

    pass moderate drinking penalty

--- src/js/expectancy.js
+++ src/js/expectancy.js
@@ -20,6 +20,8 @@ export default class Expectancy {
       life += -10;
     } else if (this.drinks >= 7) {
       life += -5
+    } else if (this.drinks >= 3 && this.drinks < 7) {
+      life += -2
     }
     return life
   }
